facbc48971fe8e88d2e6497621daa15ed37f0875
Oussama TAKI AMRANI <106884587+oussama-taki-amrani@users.noreply.github.com>
2023-11-15 15:45:47 -0500

    test commits

--- .github/workflows/myscript.py
+++ .github/workflows/myscript.py
@@ -1,3 +1,4 @@
 import os
+print("Hello world from workflow")
 os.system('git bisect start c0e76c8eef27475fc58006a140886bf0bf512ac1 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
 os.system('git bisect run python manage.py test')
